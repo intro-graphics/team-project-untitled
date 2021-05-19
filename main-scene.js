bdc088bb3162715514b52ba5391fc5cd2d8c3171
Bryan Tang <tangtang1228@ucla.edu>
2021-05-19 23:03:19 +0800

    Update main-scene.js

--- main-scene.js
+++ main-scene.js
@@ -7,6 +7,7 @@ import {Scene_To_Texture_Demo} from "./examples/scene-to-texture-demo.js"
 import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
 import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
+import {Tetris} from "./tetris.js"
 // Pull these names into this module's scope for convenience:
 const {
     Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
@@ -25,19 +26,20 @@ const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
 
 Object.assign(defs,
     {Axes_Viewer, Axes_Viewer_Test_Scene},
-            {Inertia_Demo, Collision_Demo},
-            {Many_Lights_Demo},
-            {Obj_File_Demo},
-            {Scene_To_Texture_Demo},
-            {Surfaces_Demo},
-            {Text_Demo},
-            {Transforms_Sandbox});
+    {Inertia_Demo, Collision_Demo},
+    {Many_Lights_Demo},
+    {Obj_File_Demo},
+    {Scene_To_Texture_Demo},
+    {Surfaces_Demo},
+    {Text_Demo},
+    {Transforms_Sandbox},
+    {Tetris});
 
 // ******************** End extra step
 
 // (Can define Main_Scene's class here)
 
-const Main_Scene = Transforms_Sandbox;
+const Main_Scene = Tetris;
 const Additional_Scenes = [];
 
 export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
